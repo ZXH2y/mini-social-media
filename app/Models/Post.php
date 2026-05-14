b84a830d7ca767eef10dc8236f7c521eee055f21
ZXH2y <andri9825559@gmail.com>
2026-05-14 17:41:50 +0800

    Penyempurnaan get post dengan relasi

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
@@ -14,6 +15,18 @@ use SoftDeletes;
         'content',
         'image_url'
     ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
+
+    public function likes(){
+        return $this->hasMany(Like::class);
+    }
 }
 
 
